faddd07fc9ee43ba3c210781e1c773cd60513f8b
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-26 20:16:08 +0300

    lesson19

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import timer from "./modules/timer.js";
+
+timer("24 april 2024");
+
---
+++ src/modules/timer.js
@@ -0,0 +1,29 @@
+/* eslint-disable arrow-parens */
+/* eslint-disable prefer-const */
+const timer = (deadline) => {
+    const timerHours = document.getElementById("timer-hours");
+    const timerMinutes = document.getElementById("timer-minutes");
+    const timerSeconds = document.getElementById("timer-seconds");
+
+    const getTimeRemaining = () => {
+        let dateStop = new Date(deadline).getTime();
+        let dateNow = new Date().getTime();
+        let timeRemaining = (dateStop - dateNow) / 1000; //кол-во сек. до дедлайна
+        let hours = Math.floor(timeRemaining / 60 / 60);
+        let minutes = Math.floor((timeRemaining / 60) % 60);
+        let seconds = Math.floor(timeRemaining % 60);
+
+        return { timeRemaining, hours, minutes, seconds };
+    };
+    const updateClock = () => {
+        let getTime = getTimeRemaining();
+        timerHours.textContent = getTime.hours;
+        timerMinutes.textContent = getTime.minutes;
+        timerSeconds.textContent = getTime.seconds;
+        if (getTime.timeRemaining > 0) {
+            setTimeout(updateClock, 1000);
+        }
+    };
+    updateClock();
+};
+export default timer;
