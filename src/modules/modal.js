fca5c2470b686ee0f4b4d7faff03e985b2bff328
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-27 22:04:45 +0300

    lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,14 +2,46 @@ const modal = () => {
     const modal = document.querySelector(".popup");
     const buttons = document.querySelectorAll(".popup-btn");
     const closeBtn = modal.querySelector(".popup-close");
-    console.log(buttons);
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            modal.style.display = "block";
+    const media = window.matchMedia('(min-width: 768px)');
+    
+    let count = 0;
+    let idInterval;
+
+    if (media.matches) {
+        const animatePopup = () => {
+            count++;
+            idInterval = requestAnimationFrame(animatePopup);
+            modal.style.display = 'block';
+            if (count < 100) {
+                modal.style.opacity = count + '%';
+            } else {
+                cancelAnimationFrame(idInterval);
+            }
+        };
+        const unPopup = () => {
+            count--;
+            idInterval = requestAnimationFrame(unPopup);
+
+            if (count > 0) {
+                modal.style.opacity = count + '%';
+            } else {
+                cancelAnimationFrame(idInterval);
+                modal.style.display = 'none';
+            }
+        };
+        buttons.forEach(btn => {
+            btn.addEventListener('click', animatePopup);
         });
-    });
-    closeBtn.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
+        closeBtn.addEventListener('click', unPopup);
+    } else {
+        buttons.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.style.display = 'block';
+            });
+        });
+        closeBtn.addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    }
 };
 export default modal;
