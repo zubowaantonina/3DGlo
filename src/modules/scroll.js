60c9aa6b4d4803b5373edd2e37725aecbee0b709
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-28 12:40:37 +0300

    scroll

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,23 +1,19 @@
-const scroll=()=>{
-    const menu = document.querySelector("menu");
-    const links = menu.querySelectorAll("ul>li>a");
-    const main=document.querySelector("main");
-    const mainBtn=main.querySelector("a")
-    const allLinks = [...links,mainBtn]
+const scroll = () => {
+  const menu = document.querySelector("menu");
+  const links = menu.querySelectorAll("ul>li>a");
+  const mainBtn = document.querySelector("main>a");
+  const allLinks = [...links, mainBtn];
 
-    allLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const id = link.getAttribute('href').substring(1)
-            const section = document.getElementById(id)
-          
-            if (section) {
-                section.scrollIntoView({behavior: "smooth", block: "start"})
-            
-    
-            }
-        })
-    })
+  allLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const id = link.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
 
-}
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    });
+  });
+};
 export default scroll;
