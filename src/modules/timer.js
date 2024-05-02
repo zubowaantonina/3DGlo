2c4ecd551d8f297c2ec796e37cfc3c8e23d135ad
zubowaantonina <zubowaantonina@yandex.ru>
2024-05-02 23:50:22 +0300

    new timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,33 +1,43 @@
 const timer = (deadline) => {
-    const timerHours = document.getElementById("timer-hours");
-    const timerMinutes = document.getElementById("timer-minutes");
-    const timerSeconds = document.getElementById("timer-seconds");
+  const timerHours = document.getElementById("timer-hours");
+  const timerMinutes = document.getElementById("timer-minutes");
+  const timerSeconds = document.getElementById("timer-seconds");
 
-    const getTimeRemaining = () => {
-        let dateStop = new Date(deadline).getTime();
-        let dateNow = new Date().getTime();
-        let timeRemaining = (dateStop - dateNow) / 1000; //кол-во сек. до дедлайна
-        let hours = Math.floor(timeRemaining / 60 / 60);
-        let minutes = Math.floor((timeRemaining / 60) % 60);
-        let seconds = Math.floor(timeRemaining % 60);
-        seconds < 10 ? seconds = `0${seconds}` : seconds = seconds;
-        minutes < 10 ? minutes = `0${minutes}` : minutes = minutes;
-        hours < 10 ? hours = `0${hours}` : hours = hours;
-        return { timeRemaining, hours, minutes, seconds };
-    };
-    const updateClock = () => {
-        let getTime = getTimeRemaining();
-        timerHours.textContent = getTime.hours;
-        timerMinutes.textContent = getTime.minutes;
-        timerSeconds.textContent = getTime.seconds;
-        if (getTime.timeRemaining <= 0) {
-            timerHours.textContent = "00";
-            timerMinutes.textContent = "00";
-            timerSeconds.textContent = "00";
-            clearInterval(interval);
-        }
-    };
-    updateClock();
-    let interval = setInterval(updateClock, 1000);
+  const zero = (elem) => {
+    if (elem < 10) {
+      return (elem = `0${elem}`);
+    } else {
+      return elem;
+    }
+  };
+  let dateStop = new Date(deadline).getTime();
+  const getTimeRemaining = () => {
+    let dateNow = new Date().getTime();
+    let timeRemaining = (dateStop - dateNow) / 1000; //кол-во сек. до дедлайна
+    let hours = Math.floor(timeRemaining / 60 / 60);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
+    seconds = zero(seconds);
+    minutes = zero(minutes);
+    hours = zero(hours);
+    return { timeRemaining, hours, minutes, seconds };
+  };
+ 
+  let interval;
+  const updateClock = () => {
+    let getTime = getTimeRemaining();
+    timerHours.textContent = getTime.hours;
+    timerMinutes.textContent = getTime.minutes;
+    timerSeconds.textContent = getTime.seconds;
+    if (getTime.timeRemaining <= 0) {
+      clearInterval(interval);
+      timerHours.textContent = "00";
+      timerMinutes.textContent = "00";
+      timerSeconds.textContent = "00";
+    }
+  };
+
+  interval = setInterval(updateClock, 1000);
+  updateClock();
 };
 export default timer;
